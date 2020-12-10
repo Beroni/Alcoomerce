f5f5a5a564ce2de055fc5e7503bf13841b57717a
Beroni <beronimagalhaes@gmail.com>
2020-12-10 19:23:35 -0300

    error handling

--- src/app.js
+++ src/app.js
@@ -11,12 +11,25 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.errorMiddlewares();
   }
 
   middlewares() {
     this.server.use(cors());
     this.server.use(express.json());
+  }
+
+  errorMiddlewares() {
     this.server.use(errors());
+    this.server.use((error, req, res, next) => {
+      if (error.joi) {
+        // if joi produces an error, it's likely a client-side problem
+        return res.status(400).json({
+          error: error.joi.message,
+        });
+      } // otherwise, it's probably a server-side problem.
+      return res.status(500).send(error);
+    });
   }
 
   routes() {
