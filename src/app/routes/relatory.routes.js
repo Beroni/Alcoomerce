cb3536bad229511a3433ef43b550ac71626842ba
Beroni <beronimagalhaes@gmail.com>
2020-12-10 01:33:26 -0300

    final

--- src/app/routes/relatory.routes.js
+++ src/app/routes/relatory.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-// import { celebrate, Joi, Segments } from 'celebrate';
 import authJWT from '../../middlewares/auth';
+import adminJwt from '../../middlewares/auth';
 
 import RelatoryController from '../controllers/RelatoryController';
 
 const routes = new Router();
 
-routes.get('/', authJWT, RelatoryController.userRelatory);
+routes.get('/', adminJwt, RelatoryController.userRelatory);
 routes.get('/:id', authJWT, RelatoryController.userRelatories);
 
 export default routes;
