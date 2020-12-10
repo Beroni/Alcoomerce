7eb28a4ea10cf78850ec465ca0c8ae58e016d5a6
Beroni <beronimagalhaes@gmail.com>
2020-12-10 19:24:00 -0300

    remove unused comments

--- src/app.js
+++ src/app.js
@@ -23,11 +23,9 @@ class App {
     this.server.use(errors());
     this.server.use((error, req, res, next) => {
       if (error.joi) {
-        // if joi produces an error, it's likely a client-side problem
         return res.status(400).json({
           error: error.joi.message,
         });
-      } // otherwise, it's probably a server-side problem.
       return res.status(500).send(error);
     });
   }
